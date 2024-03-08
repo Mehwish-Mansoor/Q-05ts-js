e08b38c790976b86c09d6f943854f0f421ae0e4a
Mehwish-Mansoor <160300292+Mehwish-Mansoor@users.noreply.github.com>
2024-03-08 10:06:48 +0500

    Add files via upload
    Q#05 of 45Assignment

---
+++ Qno5.js
@@ -0,0 +1,7 @@
+"use strict";
+// Famous Quote 2: Repeat Exercise 4, but this time store the famous person's name in a variable
+// called famous_person.Then compose your message ans store it in a new variable
+// called Message. print your Message. 
+let Famous_person = "Allama Aqbal";
+let message = "Be aware of your own worth, use all of your power to achieve it. Create an ocean from a dewdrop. Do not beg for light from the moon, obtain it from the spark within you.";
+console.log(`${Famous_person} once said,"${message}"`);
